1be59d80379481608d3bbffad14b9e7fa9a88233
safinhr <hrsafin2434@gmail.com>
2023-12-18 19:48:15 +0600

    implement search functionality

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { IoMdSearch } from "react-icons/io";
+import SearchResult from '../SearchResult/SearchResult';
 const Home = () => {
-    return (
-        <div  className=' flex justify-center items-center h-[65vh]'>
-            <div className=' flex flex-col items-center'>
-                <img className=' w-72' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt="" />
-                <form action="">
+    const [inputValue, setInput] = useState("")
+    const [searchResult, setSearch] = useState([])
 
-                </form>
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const result = data.filter((item) => {
+                return  (
+                  value &&
+                  item && 
+                  item.name && 
+                  item.name.toLowerCase().includes(value)
+                )
+                  
+            })
+
+            // console.log(result)
+            setSearch(result)
+        })
+    }
+
+    const handleChange = (value) => {
+        setInput(value)
+        fetchData(value)
+    }
+    return (
+        <div  className=' flex justify-center w-full items-center pt-28'>
+            <div className=' flex flex-col   '>
+                <img 
+                  className=' mb-10 mx-auto w-64' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt="" />
+                <div 
+                  className=' w-[400px] lg:w-[500px] flex mx-auto relative rounded-full  bg-gray-100'>
+                <input 
+                   value={inputValue} 
+                    className='w-full relative py-2  border border-gray-300 px-12 focus:border focus:outline-none rounded-full'      type="text" 
+                     onChange={(e) => handleChange(e.target.value)}
+                     name="" 
+                     id="" 
+                     placeholder='Search Google or type a URL' />
+                    <IoMdSearch 
+                      className=' text-gray-500 text-lg absolute mt-3.5 ml-5 ' />
+                    <div className='absolute right-0 mr-6 mt-2.5 flex items-center gap-3'>
+                    <img 
+                      className='  w-5' src="https://i.postimg.cc/52z2jpT2/google-voice.png" alt="" />
+                    <img 
+                      className=' w-5' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Google_Lens_Icon.svg/2048px-Google_Lens_Icon.svg.png" alt="" />
+                    </div> 
+                </div>
+                <div className=' px-5 '>
+                    <SearchResult searchResult={searchResult} />
+                </div>
             </div>
         </div>
     );
---
+++ src/Component/SearchResult/SearchResult.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaClockRotateLeft } from "react-icons/fa6";
+const SearchResult = ({searchResult}) => {
+    console.log(searchResult)
+    return (
+        <div className=' flex flex-col gap-6 pt-4'>
+             {searchResult.map((item) => 
+             {
+               return <div className=' flex items-center gap-4'>
+                        <span className=' text-gray-500 text-sm'>
+                            <FaClockRotateLeft />
+                         </span>
+                        <h2 className='text-sm text-gray-500'>{item.name}</h2>
+                     </div>
+             } 
+             )}
+        </div>
+    );
+};
+
+export default SearchResult;
