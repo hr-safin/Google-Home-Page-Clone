39fca865fea9a3853c9dad5a90190a62e5bc6c01
safinhr <hrsafin2434@gmail.com>
2023-12-18 15:48:21 +0600

    navigation bar added

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return (
-            <nav className=' flex justify-end items-center p-3' >
-                <ul className=' flex ju'>
-                    <li>Gmail</li>
-                    <li>Images</li>
-                    <li>Icon</li>
-                    <li>Sign In</li>
-                </ul>
-            </nav>
+  return (
+    <nav className="flex justify-end items-center p-5">
+      <ul className=" flex justify-around items-center gap-4">
         
-    );
+        <li className=" hover:underline hover:underline-offset-1">
+           <a href="https://mail.google.com/mail">
+             Gmail
+           </a>
+           
+        </li>
+        <li className=" hover:underline hover:underline-offset-1">
+           <a href="https://mail.google.com/mail">
+             Images
+           </a>
+           
+        </li>
+        <li>
+            Icon
+        </li>
+        <li>
+            <button className=" bg-blue-600 text-white rounded-md px-6 py-2">Sign In</button>
+            
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
