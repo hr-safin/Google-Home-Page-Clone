7d770f8e4342291c1b0f5a41fa8c34679b294772
safinhr <hrsafin2434@gmail.com>
2023-12-18 16:25:31 +0600

    added google logo in home page

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div  className=' flex justify-center items-center h-screen'>
-            This is home
+        <div  className=' flex justify-center items-center h-[65vh]'>
+            <div className=' flex flex-col items-center'>
+                <img className=' w-72' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt="" />
+                <form action="">
+
+                </form>
+            </div>
         </div>
     );
 };
